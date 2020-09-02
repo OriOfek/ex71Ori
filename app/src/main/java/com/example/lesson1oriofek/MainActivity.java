e923dcd3964fa06cf8028d25c14cfe39dfbe066d
OriOfek <aa6055@bs.amalnet.k12.il>
2020-09-02 13:37:24 +0300

    disply work

--- app/src/main/java/com/example/lesson1oriofek/MainActivity.java
+++ app/src/main/java/com/example/lesson1oriofek/MainActivity.java
@@ -8,24 +8,51 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    float summry;
+    EditText display;
+    String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        float summry;
-        EditText et;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = (EditText)findViewById(R.id.display);
     }
 
     public void add(View view) {
+        str = display.getText().toString();
+        str += '+';
+        display.setText(str);
+        display.setSelection(display.getText().length());
     }
 
     public void sub(View view) {
+        str = display.getText().toString();
+        str += '-';
+        display.setText(str);
+        display.setSelection(display.getText().length());
     }
 
     public void multiplication(View view) {
+        str = display.getText().toString();
+        str += 'X';
+        display.setText(str);
+        display.setSelection(display.getText().length());
     }
 
     public void divide(View view) {
+        str = display.getText().toString();
+        str += ':';
+        display.setText(str);
+        display.setSelection(display.getText().length());
+    }
+
+    public void reset(View view) {
+        str = "";
+        display.setText(str);
+        display.setSelection(display.getText().length());
+    }
+
+    public void showSummery(View view) {
     }
 }
