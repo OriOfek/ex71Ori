3f2e4acfa5d78cb6e627c8b913ef96945acaf017
OriOfek <aa6055@bs.amalnet.k12.il>
2020-09-02 14:56:55 +0300

    divide 0

--- app/src/main/java/com/example/lesson1oriofek/MainActivity.java
+++ app/src/main/java/com/example/lesson1oriofek/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.lesson1oriofek;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     float summry;
     EditText display;
+    int flag; // 1- add 2-sub 3- malitpation 4- divide
     String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,42 +17,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         display = (EditText)findViewById(R.id.display);
+        str = "";
+        flag = 1;
     }
 
     public void add(View view) {
-        str = display.getText().toString();
-        str += '+';
-        display.setText(str);
-        display.setSelection(display.getText().length());
+        if (!display.getText().toString().equals(""))
+        {
+            setSummry(Float.valueOf((display.getText().toString()).substring(str.length())));
+            str = display.getText().toString();
+            str += '+';
+            display.setText(str);
+            display.setSelection(display.getText().length());
+            flag = 1;
+        }
     }
 
     public void sub(View view) {
-        str = display.getText().toString();
-        str += '-';
-        display.setText(str);
-        display.setSelection(display.getText().length());
+        if (!display.getText().toString().equals(""))
+        {
+            setSummry(Float.valueOf((display.getText().toString()).substring(str.length())));
+            str = display.getText().toString();
+            str += '-';
+            display.setText(str);
+            display.setSelection(display.getText().length());
+            flag = 2;
+        }
     }
 
     public void multiplication(View view) {
-        str = display.getText().toString();
-        str += 'X';
-        display.setText(str);
-        display.setSelection(display.getText().length());
+        if (!display.getText().toString().equals(""))
+        {
+            setSummry(Float.valueOf((display.getText().toString()).substring(str.length())));
+            str = display.getText().toString();
+            str += 'X';
+            display.setText(str);
+            display.setSelection(display.getText().length());
+            flag = 3;
+        }
     }
 
     public void divide(View view) {
-        str = display.getText().toString();
-        str += ':';
-        display.setText(str);
-        display.setSelection(display.getText().length());
+        if (!display.getText().toString().equals(""))
+        {
+            setSummry(Float.valueOf((display.getText().toString()).substring(str.length())));
+            str = display.getText().toString();
+            str += ':';
+            display.setText(str);
+            display.setSelection(display.getText().length());
+            flag = 4;
+        }
     }
 
     public void reset(View view) {
         str = "";
+        summry = 0;
         display.setText(str);
         display.setSelection(display.getText().length());
+        flag = 1;
+        display.setHint("");
     }
 
     public void showSummery(View view) {
+        if (!display.getText().toString().equals(""))
+        {
+            if(setSummry(Float.valueOf((display.getText().toString()).substring(str.length()))))
+            {
+                display.setText(String.valueOf(summry));
+                display.setSelection(display.getText().length());
+                display.setHint("");
+            }
+        }
+        summry = 0;
+        str = "";
+        flag = 1;
+    }
+
+    public boolean setSummry(float num)
+    {
+        if (flag == 1)
+        {
+            summry +=num;
+        }
+        else if (flag == 2)
+        {
+            summry -= num;
+        }
+        else if (flag == 3)
+        {
+            summry *= num;
+        }
+        else
+        {
+            if(num == 0)
+            {
+                display.setText("");
+                display.setHint("Albert do'nt fail me");
+                return false;
+            }
+            else {
+                summry /= num;
+            }
+        }
+        return true;
     }
 }
