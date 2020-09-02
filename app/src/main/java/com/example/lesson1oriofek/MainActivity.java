d1628a12d22e74806378f9983729cbc59e2f994f
OriOfek <aa6055@bs.amalnet.k12.il>
2020-09-02 13:21:08 +0300

    bottons

--- app/src/main/java/com/example/lesson1oriofek/MainActivity.java
+++ app/src/main/java/com/example/lesson1oriofek/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.lesson1oriofek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        float summry;
+        EditText et;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(View view) {
+    }
+
+    public void sub(View view) {
+    }
+
+    public void multiplication(View view) {
+    }
+
+    public void divide(View view) {
     }
 }
